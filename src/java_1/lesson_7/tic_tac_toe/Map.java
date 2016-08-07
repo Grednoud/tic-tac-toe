328c4aaed62ba42052e22b6bb1fec35dce3c95a2
grednoud <grednoud@gmail.com>
2016-08-07 17:39:36 +0300

    2 player mode added

--- src/java_1/lesson_7/tic_tac_toe/Map.java
+++ src/java_1/lesson_7/tic_tac_toe/Map.java
@@ -23,13 +23,13 @@ public class Map extends JPanel {
 	private static final int PLAYER_2_DOT = 2;
 	
 	private static final int DRAW = 0;
-	private static final int HUMAN_WIN = 1;
-	private static final int AI_WIN = 2;
+	private static final int AI_WIN = 1;
+	private static final int HUMAN_WIN = 2;
 	private static final int HUMAN_2_WIN = 3;
 	
 	private static final String DRAW_MSG = "Ничья!";
-	private static final String HUMAN_WIN_MSG = "Победил игрок!";
-	private static final String HUMAN_2_WIN_MSG = "Победил второй игрок!";
+	private static final String HUMAN_WIN_MSG = "Победил крестик!";
+	private static final String HUMAN_2_WIN_MSG = "Победил нолик!";
 	private static final String AI_WIN_MSG = "Победил компьютер!";
 	
 	private static final Random rnd = new Random();
@@ -40,6 +40,7 @@ public class Map extends JPanel {
 	
 	private boolean initialized;
 	private boolean game_over;
+	private boolean player_1_turn;
 	private int game_over_state;
 	private int game_mode;
 	private int field_size_x;
@@ -64,11 +65,15 @@ public class Map extends JPanel {
 		if (game_over || !initialized) return;
 		int cell_x = e.getX() / cell_width;
 		int cell_y = e.getY() / cell_height;
+		int dot;
 		if (!isValidCell(cell_x, cell_y) || !isEmptyCell(cell_x, cell_y)) return;
-		field[cell_y][cell_x] = HUMAN_DOT;
+		if (player_1_turn) dot = HUMAN_DOT;
+		else dot = PLAYER_2_DOT;
+		field[cell_y][cell_x] = dot;
 		repaint();
-		if (checkWin(HUMAN_DOT)) {
-			game_over_state = HUMAN_WIN;
+		if (checkWin(dot)) {
+			if (player_1_turn) game_over_state = HUMAN_WIN;
+			else game_over_state = HUMAN_2_WIN;
 			game_over = true;
 			return;
 		}
@@ -77,17 +82,12 @@ public class Map extends JPanel {
 			game_over = true;
 			return;
 		}
-		switch (game_mode) {
-		case GAME_MODE_HUMAN_VS_AI:
-			aiTurn();
-			repaint();
-			break;
-		case GAME_MODE_HUMAN_VS_HUMAN:
-			
-			break;
-		default:
-			throw new RuntimeException("Неизвестный режим игры " + game_mode);
+		if (game_mode == GAME_MODE_HUMAN_VS_HUMAN) {
+			player_1_turn = (!player_1_turn);
+			return;
 		}
+		aiTurn();
+		repaint();
 		if (checkWin(PLAYER_2_DOT)) {
 			game_over_state = AI_WIN;
 			game_over = true;
@@ -97,7 +97,6 @@ public class Map extends JPanel {
 			game_over_state = DRAW;
 			game_over = true;
 		}
-			
 	}
 	
 	@Override
@@ -159,7 +158,8 @@ public class Map extends JPanel {
 			g.drawString(AI_WIN_MSG, 40, getHeight()/2);
 			break;
 		case HUMAN_2_WIN:
-			g.drawString(HUMAN_2_WIN_MSG, 10, getHeight()/2);
+			g.drawString(HUMAN_2_WIN_MSG, 90, getHeight()/2);
+			break;
 		default:
 			throw new RuntimeException("Неизвестный game_over_state = " + game_over_state);
 		}
@@ -174,6 +174,7 @@ public class Map extends JPanel {
             y = rnd.nextInt(field_size_y);
         } while (!isEmptyCell(x, y));
         field[y][x] = PLAYER_2_DOT;
+        player_1_turn = true;
     }
 
     private boolean detectAIWinCell(){
@@ -248,6 +249,7 @@ public class Map extends JPanel {
 		field = new int[field_size_y][field_size_x];
 		game_over = false;
 		initialized = true;
+		player_1_turn = true;
 		repaint();
 	}
 }
